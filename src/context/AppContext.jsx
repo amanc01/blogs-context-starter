0df7a41527ed54f923f757415796e3c5a2c48a8f
Aman <amanchaurasia2019@gmail.com>
2023-12-17 00:11:47 +0530

    inital

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import { createContext } from 'react';
+import { useState } from 'react';
 
 export const AppContext = createContext();
+//children <App/> component hai jo index.js me hai
+function AppContextProvider ({ children }){
+    const [loading , setLoading] = useState(false);
+    const [posts , setPosts] = useState([]);
+    const [page , setPage] = useState(1);
+    const [totalPages , setTotalPages] = useState(null);
 
-export function AppContextProvider ({ children }){
+    // "value" name hai koi bhi rakh sakte hai 
+    //sare variable ko value me store karke dusre component me use kar sakte hai
+    const value = {
+        loading,
+        setLoading,
+        posts,
+        setPosts,
+        page,
+        setPage,
+        totalPages,
+        setTotalPages
+    };
 
+    return <AppContext.Provider value={value}>
+        {children}
+        </AppContext.Provider>;
 }
